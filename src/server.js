993e9aabafefb327cbdf213ec8a90689115c002d
ehdtjs0612 <inko51366@naver.com>
2023-09-21 17:09:43 +0900

    Refactor: separate app.listen

--- src/server.js
+++ src/server.js
@@ -2,12 +2,9 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT;
 
 app.get("/", async (req, res, next) => {
     res.send("<div>gd</div>")
 });
 
-app.listen(PORT, (req, res, next) => {
-    console.log(`${PORT}번 포트에서 실행`);
-});
+module.exports = app;
