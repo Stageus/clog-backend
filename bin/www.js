f8d5ae16dc728344c239876606e1f7855c0fca8a
ehdtjs0612 <inko51366@naver.com>
2023-09-22 01:34:32 +0900

    File: add server.js

--- bin/www.js
+++ bin/www.js
@@ -3,6 +3,10 @@ require("dotenv").config();
 const app = require("../src/server");
 const PORT = process.env.PORT;
 
+app.get("/", (req, res) => {
+    res.send("ㅎㅇ?");
+});
+
 app.listen(PORT, (req, res, next) => {
     console.log(`${PORT}번 포트에서 실행`);
 });
