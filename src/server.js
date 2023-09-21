0a35068529ca3461fe5cd2649a42fb9c33ae39d6
ehdtjs0612 <inko51366@naver.com>
2023-09-22 02:24:01 +0900

    Feat: declare cookieParser global middleware

--- src/server.js
+++ src/server.js
@@ -2,10 +2,12 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
+const cookieParser = require("cookie-parser");
 
 const authApi = require("./routes/auth");
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/auth", authApi);
 
