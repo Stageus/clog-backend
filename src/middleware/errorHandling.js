1ad31173a3a4b5717b6da1f85d6f8db59fcb77d5
ehdtjs0612 <inko51366@naver.com>
2023-09-22 13:24:18 +0900

    Refactor: remove unnecessary code in errorHandling.js

--- src/middleware/errorHandling.js
+++ src/middleware/errorHandling.js
@@ -1,5 +1,3 @@
-const path = require("path");
-const CLIENT_PATH = path.join(__dirname, '../../client/pages');
 const multer = require("multer");
 
 const errorHandling = () => {
@@ -9,11 +7,6 @@ const errorHandling = () => {
         }
         // 개발환경 전용
         console.error(err);
-
-        if (err instanceof multer.MulterError) {
-            result.message = err.message;
-            return res.status(400).send(result);
-        }
 
         return res.status(err.status).send(result);
     }
