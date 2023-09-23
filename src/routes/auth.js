a64fcc8ce9eccf68e897cfb7f57b91edd7e64c90
ehdtjs0612 <inko51366@naver.com>
2023-09-23 16:13:34 +0900

    Feat: add check duplicate email api

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -11,7 +11,7 @@ router.post("/login", async (req, res, next) => {
     const result = {
         message: "",
         data: {}
-    }
+    };
 
     try {
         validate(email, "email").checkInput().checkLength(1, maxEmailLength);
@@ -50,6 +50,32 @@ router.post("/logout", (req, res, next) => {
     try {
         res.clearCookie("accessToken");
         return res.send(result);
+
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.get("/duplicate/email/:email", async (req, res, next) => {
+    const result = {
+        message: "",
+        data: {}
+    };
+    const { email } = req.params;
+
+    try {
+        validate(email, "email").checkInput().checkEmailRegex();
+
+        const sql = `SELECT id FROM account_tb WHERE email = $1`;
+        const params = [email];
+        const data = await pool.query(sql, params);
+
+        // 중복된 이메일이 존재하는 경우
+        if (data.rowCount !== 0) {
+            throw new BadRequestException("중복된 이메일이 존재합니다");
+        }
+        result.message = "사용 가능한 이메일입니다";
+        res.send(result);
 
     } catch (error) {
         next(error);
