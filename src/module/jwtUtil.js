ac327690ce1fd4b251c52cfdaf9758d5b447ed47
ehdtjs0612 <inko51366@naver.com>
2023-09-23 14:08:53 +0900

    Refactor: modify payload userPk -> userId

--- src/module/jwtUtil.js
+++ src/module/jwtUtil.js
@@ -1,9 +1,9 @@
 const jwt = require("jsonwebtoken");
 const { secretKey, accessTokenOption } = require("../../config/jwtSetting");
 
-const userSign = async (user) => {
+const userSign = (user) => {
     const payload = {
-        userPk: user.id,
+        userId: user.id,
     }
 
     return jwt.sign(payload, secretKey, accessTokenOption);
